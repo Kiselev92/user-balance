e00316acba11b1054e893e0717232239f2a784a9
Anton Kiselev <kiselev-as92@yandex.ru>
2025-05-19 21:00:39 +0300

    исправление

--- target/generated-sources/annotations/com/kiselev/userbalance/model/mapper/PhoneDataMapperImpl.java
+++ target/generated-sources/annotations/com/kiselev/userbalance/model/mapper/PhoneDataMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-19T00:00:34+0300",
+    date = "2025-05-19T20:59:50+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.15 (Amazon.com Inc.)"
 )
 @Component
--- target/generated-sources/annotations/com/kiselev/userbalance/model/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/kiselev/userbalance/model/mapper/UserMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-19T11:26:43+0300",
+    date = "2025-05-19T20:59:50+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.15 (Amazon.com Inc.)"
 )
 @Component
